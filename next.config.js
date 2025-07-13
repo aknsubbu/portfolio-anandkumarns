cbf16480f6c5fe79bffdfab454b8faaaac5a0f3d
aknsubbu <aknsubbu@gmail.com>
2025-07-13 11:27:57 +0530

    Update Next.js configuration and package.json: add ESLint plugin for Next.js and adjust NextUI dependencies.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  extends: [
+    //...
+    "plugin:@next/next/recommended",
+  ],
+};
 
 module.exports = nextConfig;
